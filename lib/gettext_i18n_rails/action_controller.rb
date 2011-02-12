858b68fb4a81b87efa1af044de6c323552e29d1d
Nikos Dimitrakopoulos <nospam@nikosd.com>
2011-02-12 20:18:59 +0200

    Temporary bug-fix/hack for bug introduced in Rails 3.0.4.

--- lib/gettext_i18n_rails/action_controller.rb
+++ lib/gettext_i18n_rails/action_controller.rb
@@ -1,3 +1,6 @@
+# See Rails 3.0.4 bug https://rails.lighthouseapp.com/projects/8994/tickets/6393
+require 'active_support/core_ext/module/deprecation'
+
 class ActionController::Base
   def set_gettext_locale
     requested_locale = params[:locale] || session[:locale] || cookies[:locale] ||  request.env['HTTP_ACCEPT_LANGUAGE']
